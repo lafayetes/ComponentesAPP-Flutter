820f6de28012a0087b3bfb16ded7bd60cef3fbdf
DiegoLaFaye <lafayediego@gmail.com>
2021-07-16 22:33:42 -0400

    Agregado estilos a las cards. Para mejor manejo se modifico una card a container y se añadieron propiedades de estilo

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -13,7 +13,21 @@ class CardPage extends StatelessWidget {
         // En esta parte del codigo agregamos un metodo que devolvera un widget
         children: [
           _cardTipo1(),
+          SizedBox(height: 30),
           _cardTipo2(),
+          SizedBox(height: 30),
+          _cardTipo1(),
+          SizedBox(height: 30),
+          _cardTipo2(),
+          SizedBox(height: 30),
+          _cardTipo1(),
+          SizedBox(height: 30),
+          _cardTipo2(),
+          SizedBox(height: 30),
+          _cardTipo1(),
+          SizedBox(height: 30),
+          _cardTipo2(),
+          SizedBox(height: 30),
         ],
       ),
     );
@@ -23,6 +37,9 @@ class CardPage extends StatelessWidget {
   Widget _cardTipo1() {
     //El widget de tipo Card te permite crear como su nombre lo dice una tarjeta que le da un estilo al componente que se desea realizar. Tiene varias propiedades pero en este se ultiza el child
     return Card(
+      //Aqui agregamos un poco de estilo a la card, colocando elevacion y forma para darle mejor estilo
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
       child: Column(
         children: [
           ListTile(
@@ -50,19 +67,48 @@ class CardPage extends StatelessWidget {
   }
 
   Widget _cardTipo2() {
-    return Card(
+    //Aqui modificamos el card a un container para que la imagen s epueda adaptar a los bordes
+    final card = Container(
       child: Column(
         children: [
           FadeInImage(
-              placeholder: AssetImage('assets/jar-loading.gif'),
-              image: NetworkImage(
-                  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Landscape_Arnisee-region.JPG/1200px-Landscape_Arnisee-region.JPG')),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Landscape_Arnisee-region.JPG/1200px-Landscape_Arnisee-region.JPG'),
+            fadeInDuration: Duration(milliseconds: 200),
+            // Con esta propiedad de fit podemos hacer que la imagen se adapte al contenedor y asi evitar que los bordes de vayan fuera
+            fit: BoxFit.cover,
+            height: 300,
+          ),
+
           // Image(
           //   image: NetworkImage(
           //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Landscape_Arnisee-region.JPG/1200px-Landscape_Arnisee-region.JPG'),
           // ),
           Text('Nose que colocar de titulo')
         ],
+      ),
+    );
+    return Container(
+      //La decoracion te permite colocar diferentes propiedades para darle un estilo al container. Esta el border radius que te permite darle un borde en nuestro caso redondeado, un color, sombras, etc
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: Colors.white,
+          //Para el box Shadow manejamos una lista de tipo BoxShadow que puede llevar las propiedades color, blurRadius, SpreadRadius y el offset. Este ultimo lo que hace es ubicar la sombra en base a las coordenadas que se colocan tomandolo como un plano cartesiano
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 2.0,
+                offset: Offset(
+                  1,
+                  5,
+                ),
+                spreadRadius: 2.0),
+          ]),
+      //Con este clipRRect se corta todo lo que se este saliendo de nuestro container dandole un borde circular
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: card,
       ),
     );
   }
