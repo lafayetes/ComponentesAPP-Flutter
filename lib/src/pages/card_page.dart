83754e7d8084fc1d2c590886bb296755eaedff3a
DiegoLaFaye <lafayediego@gmail.com>
2021-07-16 21:49:40 -0400

    Curso sobre cards, manejo de imagenes y placeholders

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,12 +9,19 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
-        children: [_cardTipo1()],
+
+        // En esta parte del codigo agregamos un metodo que devolvera un widget
+        children: [
+          _cardTipo1(),
+          _cardTipo2(),
+        ],
       ),
     );
   }
 
+  // Aqui estaria el widget _cardTipo1 en el que se colocaria sus correspondientes propiedades
   Widget _cardTipo1() {
+    //El widget de tipo Card te permite crear como su nombre lo dice una tarjeta que le da un estilo al componente que se desea realizar. Tiene varias propiedades pero en este se ultiza el child
     return Card(
       child: Column(
         children: [
@@ -37,6 +44,24 @@ class CardPage extends StatelessWidget {
               ),
             ],
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: [
+          FadeInImage(
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              image: NetworkImage(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Landscape_Arnisee-region.JPG/1200px-Landscape_Arnisee-region.JPG')),
+          Image(
+            image: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Landscape_Arnisee-region.JPG/1200px-Landscape_Arnisee-region.JPG'),
+          ),
+          Text('Nose que colocar de titulo')
         ],
       ),
     );
